e0aa2c5d5e6689c312feb39432c6ec73cb110984
DrSuavidade <rafazercoconascalcas4@gmail.com>
2025-10-20 13:47:37 +0100

    BackWeapons

---
+++ Assets/Scripts/Gameplay/Abilities/EssenceAbility.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Geneforge.Gameplay.Weapons.Stats;
+using Geneforge.Gameplay.Weapons.Bullets;
+using Geneforge.Gameplay.Characters.Enemies;
+
+namespace Geneforge.Gameplay.Abilities
+{
+    public abstract class EssenceAbility : ScriptableObject
+    {
+        public virtual void OnBulletSpawn(Bullet bullet, WeaponStats activeStats) {}
+        public virtual void OnHitEnemy(Bullet bullet, Enemy enemy, WeaponStats activeStats) {}
+    }
+}
